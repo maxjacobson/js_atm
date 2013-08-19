c4de9731f167bfab1926449a7095892707b0f097
Max Jacobson <maxwell.jacobson@gmail.com>
2013-08-19 12:09:55 -0400

    refactor

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-// alert, prompt, confirm
-
 var total
   , command
   , amount
   , transact
   , looping = true;
 
-total = parseFloat(prompt("How much money do you have? (be honest)"));
-
-transact = function(action, amt) {
-  console.log(total);
-  if(!isNaN(amt)) {
+transact = function(action) {
+  amount = parseFloat(prompt("How much would you like to " + action));
+  if(isNaN(amount)) {
+    return false;
+  } else {
     switch(action) {
       case "deposit":
-        total += amt;
+        total += amount;
         break;
       case "withdraw":
-        total -= amt;
+        if(amount > total) {
+          alert("Sorry, you don't have that much");
+        } else {
+          total -= amount;
+        }
         break;
       default:
         return false;
         break;
     }
-  } else {
-    return false;
   }
 };
 
+total = parseFloat(prompt("How much money do you have? (be honest)"));
+
 while(looping == true) {
-  alert("You have $" + total);
   command = prompt("What would you like to do, deposit, withdraw, or exit?").toLowerCase();
   if(command === "deposit") {
-    amount = parseFloat(prompt("How much would you like to deposit?"));
-    transact("deposit", amount);
+    transact("deposit");
   } else if(command === "withdraw") {
-    amount = parseFloat(prompt("How much would you like to withdraw?"));
-    transact("withdraw", amount);
+    transact("withdraw");
   } else if(command === "exit") {
     looping = false;
   } else {
     alert("Unrecognized command");
   }
+  alert("You have $" + total);
 }
